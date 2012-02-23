90809b13ab090d7ebc06e708a1aedccf503da3da
Ahmad Salim <asal@itu.dk>
2012-02-23 18:51:00 +0100

    Changed lexer tokens from ALLCAPS to PrefixedWith_t

--- TestScript.fsx
+++ TestScript.fsx
@@ -7,5 +7,5 @@ let interpret =
     printfn "%A" << run << fromString
 
 interpret (" let even n = if n = 0 then true else odd(n-1)
-             and odd  n = if n = 1 then true else even(n-1) 
-             in let even n = n + 1 in even 53353 end end")
+             and odd  n = if n = 0 then false else even(n-1) 
+             in even 55535 end")
